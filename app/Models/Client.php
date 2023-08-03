55b9622bdd6dd61f2b10137ea676c8491e468c25
Marcelo Caeatno <marcelloinfoweb@gmail.com>
2023-08-03 09:29:15 -0300

    Desenvolvimento de um CRUD de clientes - Index

--- app/Models/Client.php
+++ app/Models/Client.php
@@ -6,7 +6,6 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasMany;
-use Illuminate\Database\Eloquent\Relations\HasOne;
 
 class Client extends Model
 {
@@ -17,9 +16,9 @@ class Client extends Model
         'user_id',
     ];
 
-    public function address(): HasOne
+    public function address(): belongsTo
     {
-        return $this->hasOne(Address::class);
+        return $this->belongsTo(Address::class);
     }
 
     public function user(): BelongsTo
