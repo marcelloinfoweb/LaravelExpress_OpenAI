574a2a3307304bdda8cd3a8f73b886ab0cbc9951
Marcelo Caeatno <marcelloinfoweb@gmail.com>
2023-08-16 17:07:05 -0300

    Integração da OpenAI na aplicação

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\ClientController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use OpenAI\Laravel\Facades\OpenAI;
 
 /*
 |--------------------------------------------------------------------------
@@ -29,6 +30,22 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 
     Route::resource('/clients', ClientController::class);
+
+    Route::get('/chart', function () {
+        $fields = implode(',', \App\Models\SalesCommission::getColumns());
+        $question = 'Gere um gráfico das vendas por empresa no eixo y ao longo dos últimos 5 anos';
+
+        $prompt = "Considerando a lista de campos ($fields), e gere uma configuração json do vega-lite v5 ";
+        $prompt .= "(sem campo de dados e com descrição) que atenda ao seguinte pedido: $question";
+
+        $config = OpenAI::completions()->create([
+            'model' => 'text-davinci-003',
+            'prompt' => $prompt,
+            'max_tokens' => 1500
+        ])->choices[0]->text;
+
+        dd($config);
+    });
 });
 
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
